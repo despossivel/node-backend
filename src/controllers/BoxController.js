e67f19e8151608298508df71c6b446b41740b2af
matheus <matheus@ti>
2019-04-09 18:05:11 -0300

    backend

---
+++ src/controllers/BoxController.js
@@ -0,0 +1,27 @@
+const Box = require('../models/Box');
+
+class BoxController{
+    async store(req,res){
+            const box = await Box.create(req.body);
+        
+        return res.json(box);
+    }
+
+    async show(req, res){ 
+        
+        const box = await Box.findById(req.params.id).populate({
+            path:'Files',
+            options:{
+                sort:{
+                    createdAt:-1
+                }
+            }
+        });
+
+        return res.json(box);
+    }
+
+
+}
+
+module.exports = new BoxController();
---
+++ src/models/Files.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+
+const Files = new mongoose.Schema({
+    title:{
+        type:String,
+        required:true
+    },
+    path:{
+        type:String,
+        required:true
+    },
+    files:[]
+},{
+    timestamps:true,
+    toObject:{virtuals:true},
+    toJSON:{virtuals:true}
+});
+
+
+Files.virtual('url').get(function(){
+    return `http://localhost:3333/files/${encodeURIComponent(this.path)}`
+})
+module.exports = mongoose.model("Files", Files);
