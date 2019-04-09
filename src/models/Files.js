3357618df91dfea7a4843b811e0d5f557e519c5d
matheus <matheus@ti>
2019-04-09 18:09:52 -0300

    variaveis de ambiente

--- src/models/Files.js
+++ src/models/Files.js
@@ -18,6 +18,7 @@ const Files = new mongoose.Schema({
 
 
 Files.virtual('url').get(function(){
-    return `http://localhost:3333/files/${encodeURIComponent(this.path)}`
+    const url = process.env.URL || 'http://localhost:3333';
+    return `${url}/files/${encodeURIComponent(this.path)}`
 })
 module.exports = mongoose.model("Files", Files);
